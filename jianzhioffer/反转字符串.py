8db6733f5c35374fe1a5750b43d62292ba1b7592
zhuangjie <zhuangjie1108@cashbus.com>
2019-08-27 14:37:42 +0800

    update

--- jianzhioffer/反转字符串.py
+++ jianzhioffer/反转字符串.py
@@ -1,7 +1,7 @@
 def reverse_str(s):
     str_list = list(s)
     length = len(str_list)
-    reverse_str_part(str_list,0,length-1)
+    reverse_str_part(str_list, 0, length - 1)
     i = 0
     print(str_list)
     while i < length:
@@ -10,11 +10,12 @@ def reverse_str(s):
             end = i + 1
             while end < length and str_list[end] != ' ':
                 end += 1
-            reverse_str_part(str_list, start, end)
+            reverse_str_part(str_list, start, end - 1)
             i = end + 1
         else:
             i += 1
     print(''.join(str_list))
+
 
 def reverse_str_part(s, start, end):
     while start < end:
